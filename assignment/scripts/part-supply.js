41e117b6d4c44e389c97d5fde6e14cbec2e2c880
Tucker Melssen <tuckerjmelssen@gmail.com>
2023-11-09 18:11:39 -0600

    assignment complete

--- assignment/scripts/part-supply.js
+++ assignment/scripts/part-supply.js
@@ -5,26 +5,31 @@ console.log('****** Part Supply *******');
 
 // 1. Create a variable called 'partsNeeded' with a value of the number 40.
 console.log('1. Create partsNeeded:');
-
+let partsNeeded = 40;
+console.log(partsNeeded);
 
 // 2. Create a variable called 'supplyChanges' whose value is an array containing
 //    the following numbers: 3, 5, -6, 0, 7, 11.
 console.log('2. Create supplyChanges:');
-
+let supplyChanges = [3, 5, -6, 0, 7, 11];
+console.log(supplyChanges);
 
 // 3. Create a variable called 'secondItem' and assign it the value of the second
 //    item in the 'supplyChanges' array.
 console.log('3. Access the second value of supplyChanges:');
-
+let secondItem = supplyChanges[1];
+console.log(secondItem);
 
 // 4. The last value in the 'supplyChanges' array was added by mistake.
 //    Remove it from the array and store it inside a new variable called 'removedItem'.
 console.log('4. Remove the last value from supplyChanges:');
-
+let removedItem = supplyChanges.pop();
+console.log(removedItem);
 
 // 5. A delivery of 25 more parts arrived. Add the value 25 to the end of the array
 console.log('5. Add the value 25 into supplyChanges.');
-
+supplyChanges.push(25);
+console.log(supplyChanges);
 
 // 6. Create three new variables named 'positives', 'negatives', and
 //    'zeroes' whose values are empty arrays. Then, write a for loop
@@ -34,22 +39,54 @@ console.log('5. Add the value 25 into supplyChanges.');
 //      - If the value is a negative number, push it into the 'negatives' array.
 //      - If the value is a zero, push it into the 'zeroes' array.
 console.log('6. Looping through supplyChanges to populate arrays with positive, negative, and zero values:');
+let positives = [];
+let negatives = [];
+let zeroes = [];
 
-
-
+for(let i=0; i<supplyChanges.length; i++){
+  if(supplyChanges[i]>0){
+    positives.push(supplyChanges[i]);
+  } else if(supplyChanges[i]<0){
+    negatives.push(supplyChanges[i]);
+  } else {
+    zeroes.push(supplyChanges[i]);
+  }
+}
+console.log('Postives:', positives);
+console.log('Negatives:', negatives);
+console.log('Zeroes', zeroes);
 
 // ***** STRETCH GOALS *********************************************
 // 7. Rewrite the 'for' loop from #6 as a 'for...of' loop. Instead of 'positives',
 //    'negatives', and 'zeroes', create three new arrays named 'stretchPositives',
 //    'stretchNegatives', and 'stretchZeroes'. 
 console.log('7. Looping through supplyChanges to populate more arrays with positive, negative, and zero values:');
+let stretchPositives = [];
+let stretchNegatives = [];
+let stretchZeroes = [];
 
+for(let supplies of supplyChanges){
+  if(supplies>0){
+    stretchPositives.push(supplies);
+  } else if(supplies<0){
+    stretchNegatives.push(supplies);
+  } else {
+    stretchZeroes.push(supplies);
+  }
+}
+console.log('Stretch Positives:', stretchPositives);
+console.log('Stretch Negatives:', stretchNegatives);
+console.log('Stretch Zeroes:', stretchZeroes);
 
 // 8. Create a variable called 'totalParts' and assign it a value of 0.
 //    Then, write a loop that adds each value of the 'supplyChanges'
 //    array to the 'totalParts' variable.
 console.log('8. Looping through supplyChanges to calculate the sum:');
-
+let totalParts = 0;
+for(let supplies of supplyChanges){
+  totalParts+=supplies;
+}
+console.log(totalParts);
 
 // 9. We have a large stash of parts in our warehouse that we 
 //    need to box up and get ready for shipment. There are 572
@@ -63,9 +100,16 @@ console.log('8. Looping through supplyChanges to calculate the sum:');
 //    "left over," and the value of 'boxesFilled' should reflect
 //    how many boxes were filled.
 console.log('9. Filling boxes with a "while" loop');
+let parts = 572;
+let boxesFilled = 0;
 
-
-
+while(parts>7){
+  parts -= 7;
+  boxesFilled++;
+}
+console.log('Boxes Filled:', boxesFilled);
+console.log('Left Over:', parts);
+console.log(572%7);
 
 // DO NOT MODIFY
 // Used for automated testing
